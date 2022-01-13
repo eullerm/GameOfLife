2b63d8ff74ba9dbba58d5ef070933cd637ab98c8
Euller Macena <49864491+eullerm@users.noreply.github.com>
2022-01-13 18:05:08 -0300

    Finalização do trabalho
    * Jogo funcional
    * Imprimindo a matriz resultante e o numero de iterações até normalizar ela.
    
    Co-Authored-By: MalkaiOliveira <62706524+MalkaiOliveira@users.noreply.github.com>

--- src/Main.hs
+++ src/Main.hs
@@ -1,107 +1,77 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module Main where
 import Data.List
 import Data.List.Split
 import Data.Typeable
-import Data.Char (ord, chr)
+import GHC.Arr
+import System.IO
+import Control.Monad
+import Data.Char
 
-width, height :: Int
-width = 80 -- tam da linha
-height = 24 -- tam da coluna
-
-type Cells = [Pos] -- coordenadas das células
-
-data Cell = V | M | Z deriving Show -- Define como uma célula pode ser
-
-rightList (x:y:rest) = (x,y) : rightList (y:rest)
-rightList _ = []
-
-right m = m >>= rightList
-
-swap (x,y) = (y,x)
-co direction = map swap . direction
-left = co right
-
-down = right . transpose
-up   = co down
-
-downRight (xs:ys:rest) = zip xs (drop 1 ys) ++ downRight (ys:rest)
-downRight _            = []
-upLeft = co downRight
-
-upRight  = downRight . reverse
-downLeft = co upRight
-
-allDirections = [right, left, up, down,
-                 downRight, upLeft, upRight, downLeft]
-neighbors m = allDirections >>= ($m) 
-
--- Retorna a posição de um elemento x numa lista
-positions  :: Eq a => a -> [a] -> [Int]
-positions x = map fst . filter ((x ==) . snd) . zip [0..]
-
--------------------------------------------------------------
-
-type Pos = (Int,Int) -- coluna, linha
-type Row = [(Pos, String)]
+type Row = [String]
 type Matrix = [Row]
 
+-- Transforma os caracteres em upperCase
+uppercase :: String -> String
+uppercase = map toUpper
+
 -- Cria a matriz
-initMatrix :: [Int] -> IO Matrix
-initMatrix [1,width] = 
-  do 
-    line <- getLine
-    let lineSplit = words line -- Splita a linha de input do usuario
-    let exactSize = getExactSize lineSplit 1 width
-    return [exactSize]
-initMatrix [height,width] =
+initMatrix :: [Int] -> Int -> IO Matrix
+initMatrix [1, width] row =
   do
     line <- getLine
-    let lineSplit = words line -- Splita a linha de input do usuario
-    let exactSize = getExactSize lineSplit height width
-    nextLine <- initMatrix [(height-1), width]
+    let lineSplit = words (uppercase line) -- Splita a linha de input do usuario
+    let exactSize = getExactSize lineSplit row width
+    return [exactSize]
+initMatrix [height, width] row =
+  do
+    line <- getLine
+    let lineSplit =  words (uppercase line)  -- Splita a linha de input do usuario
+    let exactSize = getExactSize lineSplit row width
+    nextLine <- initMatrix [height-1, width] (row + 1)
     return (exactSize:nextLine)
 
 -- Calcula o tamanho exato que cada vetor da matriz precisa ter
 getExactSize :: [String] -> Int -> Int -> Row
-getExactSize lineSplit row columns= 
+getExactSize lineSplit row columns=
   do
-    let vetRow = take columns (repeat row)
-    let vetColumn = [1..columns]
-    let pos = zip vetRow vetColumn 
     let exactSize = take columns lineSplit
-    let incrementSize = lineSplit ++ take (columns - (length lineSplit)) (repeat "M")
-    if(length lineSplit == columns)
-      then (zip pos exactSize)
-    else if(length lineSplit < columns)
-      then (zip pos incrementSize)
+    let incrementSize = lineSplit ++ replicate (columns - length lineSplit) "M"
+    if length lineSplit == columns
+      then exactSize
+    else if length lineSplit < columns
+      then incrementSize
     else
-      zip pos exactSize
+      exactSize
 
 -- Imprime a matriz
 printMatrix :: Matrix -> IO ()
-printMatrix (vet:[]) = 
-  do 
+printMatrix [] =
+  do
+    return ()
+printMatrix [vet] =
+  do
     printValue vet
 printMatrix (vet:rest) =
-  do 
+  do
     printValue vet
     printMatrix rest
 
 -- Imprime a linha da matriz
 printValue :: Row -> IO()
-printValue [] = do putStrLn("|")
-printValue (((r,c), value):rest) =
+printValue [] = putStrLn "|"
+printValue (value:rest) =
   do
-    putStr ("| ")
-    putStr(id value)
-    putStr(" ")
-    printValue rest 
+    putStr "| "
+    putStr value
+    putStr " "
+    printValue rest
 
-printDash :: Int -> [Char] -> IO()
-printDash 0 char = do putStrLn(char)
+printDash :: Int -> String -> IO()
+printDash 0 char = putStrLn char
 printDash n char =
   do
-    putStr(char)
+    putStr char
     printDash (n-1) char
 
 -- Converte string para inteiros
@@ -112,32 +82,116 @@ stringToInts = map read
 intsToString :: [Int] -> String
 intsToString = map chr
 
+first :: (a, b, c) -> a
+first (a,_,_) = a
+second :: (a, b, c) -> b
+second (_,b,_) = b
+third :: (a, b, c) -> c
+third (_,_,c) = c
+
+-- Conta os vizinhos e verifica se são vivos, mortos ou zumbis
+countNeighbors ::[[Char]] -> Int -> Int -> [[Int]] -> (Int, Int, Int) -> (Int, Int, Int)
+countNeighbors matrix row column [] (a,b,c) = (a,b,c)
+countNeighbors matrix row column indexes (a,b,c)
+    | i < 0 || j < 0 = countNeighbors matrix row column body (a, b, c)
+    | i >= row || j >= column = countNeighbors matrix row column body (a, b, c)
+    | matrix!!index == "M" = countNeighbors matrix row column body (a, b + 1, c)
+    | matrix!!index == "V" = countNeighbors matrix row column body (a + 1, b, c)
+    | matrix!!index == "Z" = countNeighbors matrix row column body (a, b, c + 1)
+    where body = tail indexes
+          i = head indexes !! 0
+          j = head indexes !! 1
+          index = i * column + j
+
+-- Pega todos os indices adjacentes 
+getindexes :: Int -> Int -> [[Int]]
+getindexes i j = [[i-1,j+1], [i,j+1], [i+1,j+1],[i-1,j], [i+1,j],[i-1,j-1],[i, j-1], [i+1,j-1]]
+
+-- Função auxiliar para buscar os índices e contar os estados
+adz :: [[Char]] -> Int -> Int -> Int -> (Int, Int, Int)
+adz matrix row column i = countNeighbors matrix row column (getindexes (div i column) (mod i column)) (0,0,0)
+
+-- Classifica uma posição como viva, morta ou zumbi
+aliveDeadOrZombie :: [[Char]] -> Int -> Int -> Int -> [Char]
+aliveDeadOrZombie n row column i
+   | n!!i == "M" && a == 3 = "V"
+   | n!!i == "V" && z >= 1 = "Z"
+   | n!!i == "V" && a < 2  = "M"
+   | n!!i == "V" && a > 3  = "M"
+   | n!!i == "Z" && a == 0 = "M"
+   | otherwise = n!!i
+   where a = first (adz n row column i)
+         d = second (adz n row column i)
+         z = third (adz n row column i)
+
+-- Percorre todos as posições da tabela passada, classificando cada posição e adicionando na lista 'b'
+travel :: [[Char]] -> Int -> Int -> [[Char]] -> Int -> [[Char]]
+travel n row column newVet i
+   | i >= row * column = newVet
+   | otherwise = travel n row column c (i + 1)
+   where c = (aliveDeadOrZombie n row column i):newVet
+
+-- Quantas vezes percorrer toda a tabela e funcao checa se: tabela atual == tabela passada
+gameOfLife :: [[Char]] -> [Int] -> Int -> Int -> (Int, [[Char]])
+gameOfLife matrix [row, column] iteration total
+  | iteration > 0 && newMatrix /= matrix = gameOfLife newMatrix [row, column] (iteration - 1) total -- Se não: continuar execução
+  | iteration > 0 && newMatrix == matrix = ((total - iteration), matrix) -- Se sim: parar e retornar qtd de rodadas ate o momento e tabela atual
+  | otherwise = ((total - iteration), matrix) -- Se chegar ao fim do numero total de iteracoes sem estabilizar: retorna estado final da tabela
+  where newMatrix = reverse (travel matrix row column [] 0)
+
+-- Faz o merge de todos os vetores da matriz
+mergeAll :: Ord a => [[a]] -> [a]
+mergeAll [] = []
+mergeAll x = head $ mergePairs x
+
+mergePairs :: Ord a => [[a]] -> [[a]]
+mergePairs [] = []
+mergePairs (x:[]) = [x]
+mergePairs (x:y:tail) = mergePairs ((mergeTwo x y):(mergePairs tail))
+
+mergeTwo :: Ord a => [a] -> [a] -> [a]
+mergeTwo x [] = x
+mergeTwo [] x = x
+mergeTwo x y = x++y
+
+
+
 main :: IO ()
 main = do
 
-  putStrLn ("Tamanho da grid (Ex: 3x2): ")
+  putStrLn "Tamanho da grid (Ex: 3x2): "
   gridSize <- getLine
-  let gridSizeAsNumber = stringToInts  (splitOn "x" gridSize)
+  let griSizeUpperCase = uppercase gridSize
+  let gridSizeAsNumber = stringToInts (splitOn "X" griSizeUpperCase)
 
-  putStrLn ("Celulas da grid separadas por espaços (V = Viva, M = Morta, Z = Zumbi): ")
-  matrix <- initMatrix gridSizeAsNumber
+  putStrLn "Celulas da grid separadas por espaços (V = Viva, M = Morta, Z = Zumbi): "
+  matrix <- initMatrix gridSizeAsNumber 0
 
-  putStrLn ("Número de iterações máxima: ")
+  putStrLn "Número de iterações máxima: "
   iteration <- getLine
   let iterationAsNumber = read iteration :: Int
 
-  putStrLn ("Tamanho da grade: " ++ gridSize)
-
-  putStrLn ("Matriz: \n" ++ show matrix)
-  putStrLn ("\nTipo da matriz: " ++ show (typeOf matrix))
-  -- putStrLn ("Matriz com sort: \n" ++ show (sort (neighbors matrix)))
-  putStrLn ("\nPrint criado para matriz:")
-  printDash (2*4) "-"
+  putStrLn "\nMatriz inicial:"
+  printDash (4 * gridSizeAsNumber!!1 ) "-"
   printMatrix matrix
-  printDash (2*4) "-"
+  printDash (4 * gridSizeAsNumber!!1 ) "-"
+
+  let vetOfStatus = mergeAll matrix
+
+  let game = gameOfLife vetOfStatus gridSizeAsNumber iterationAsNumber iterationAsNumber
+
+  let iterationUntilNormalize = fst game
+  let vetResult = snd game
+
+  putStrLn ("\nNúmero de iterações até normalizar a matriz: " ++ show iterationUntilNormalize)
+
+  let matrixResult = chunksOf (gridSizeAsNumber!!1) vetResult
+
+  putStrLn "\nMatriz resultante:"
+  printDash (4 * gridSizeAsNumber!!1 ) "-"
+  printMatrix matrixResult
+  printDash (4 * gridSizeAsNumber!!1 ) "-"
   
-  putStrLn ("Iteração: " ++ iteration)
 
-  let s  = positions 'L' "foobaraboof"
 
-  putStrLn (show s) 
+
